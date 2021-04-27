04a864a978934e834f64e0fa757ca16bf4f4c01c
martin-alem <alemajohmartin@gmail.com>
2021-04-27 11:22:44 -0400

    implement extractWeatherData, formatWeatherData and makeRequest

--- src/js/model/weatherManager.js
+++ src/js/model/weatherManager.js
@@ -1,19 +1,20 @@
 export class WeatherManager{
 
-    #weather;
+    #weatherData;
     #currentWeatherLocation;
+    #rawData;
 
     fetchDataOnPageLoad(){
 
-        const location = this.getDefaultLocation();
+        const location = this.#getDefaultLocation();
 
         if(location){
-            this.getWeatherData({type: "pageLoad", data: location});
+            this.#getWeatherData({type: "pageLoad", data: location});
         }
         else{
-            this.getCurrentLocation()
+            this.#getCurrentLocation()
             .then(currentLocation => {
-                this.getWeatherData({type: "pageLoad", data: currentLocation});
+                this.#getWeatherData({type: "pageLoad", data: currentLocation});
                 this.setDefaultLocation(`${currentLocation[0]},${currentLocation[1]}`);
             })
             .catch(error => {
@@ -22,7 +23,7 @@ export class WeatherManager{
         }
     }
 
-    getDefaultLocation(){
+    #getDefaultLocation(){
 
         if(window.localStorage.getItem("defaultLocation")){
             const defaultLocation = window.localStorage.getItem("defaultLocation").split(",");
@@ -34,7 +35,7 @@ export class WeatherManager{
     }
 
     // gets the user current location
-    getCurrentLocation(){
+    #getCurrentLocation(){
 
         return new Promise((resolve, reject) => {
 
@@ -61,28 +62,68 @@ export class WeatherManager{
         }
     }
 
-    getWeatherData(request){
+    #getWeatherData(request){
 
         if(request.type === "pageLoad"){
             const lat = request.data[0];
             const lon = request.data[1];
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=aac304093f797550435d2ed3dad3f25b`;
-
-            fetch(url).then(result => {
-                if(result.ok){
-                    result.json().then(data => console.log(data))
-
-                }else{
-                    console.log("Error")
-                }
-            })
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=aac304093f797550435d2ed3dad3f25b&units=metric`;
+            
+            this.#makeRequest(url);
         }
 
         else if(request.type === "submitForm"){
             const city = request.data;
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=aac304093f797550435d2ed3dad3f25b`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=aac304093f797550435d2ed3dad3f25b&units=metric`;
+            
+            this.#makeRequest(url);
         }
     }
 
+    #makeRequest(url){
 
+        fetch(url)
+        .then(response => {
+            if(response.ok){
+                response.json()
+                .then(data => {
+                    this.#rawData = data
+                    this.#extractWeatherData();
+                })
+                .catch(error => console.log("Could not convert to json: "+ error));
+            }
+        })
+        .catch(error => console.log("Request Failed: "+error));
+    }
+
+    #extractWeatherData(){
+        const weatherData = {
+            temparature: this.#toNDecimalPlaces(this.#toFarienhiet(this.#rawData.main.temp), 10),
+            minTemperature: this.#toNDecimalPlaces(this.#toFarienhiet(this.#rawData.main.temp_min), 100),
+            maxTemperature: this.#toNDecimalPlaces(this.#toFarienhiet(this.#rawData.main.temp_max),100),
+            feelsLike: this.#toNDecimalPlaces(this.#toFarienhiet(this.#rawData.main.feels_like),100),
+            pressure: this.#rawData.main.pressure,
+            humidity: this.#rawData.main.humidity,
+            description: this.#rawData.weather[0].description,
+            visibility: this.#rawData.visibility,
+            windSpeed: this.#rawData.wind.speed,
+            country: this.#rawData.sys.country,
+            city: this.#rawData.name,
+            coords: this.#rawData.coord
+
+        }
+
+        this.#weatherData = weatherData;
+        console.log(this.#weatherData);
+    }
+
+    #toFarienhiet(celsius){
+        return (celsius * (9/5)) + 32;
+    }
+
+    #toNDecimalPlaces(number, decimalPlaces){
+
+        const tmpNum = Number.parseInt(number * decimalPlaces);
+        return tmpNum / decimalPlaces;
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,12 @@ module.exports = {
               loader: 'babel-loader',
               options: {
                 presets: ['@babel/preset-env'],
-                plugins: ['@babel/plugin-proposal-object-rest-spread','@babel/plugin-proposal-class-properties']
+                plugins: [
+                  '@babel/plugin-proposal-object-rest-spread',
+                  '@babel/plugin-proposal-class-properties', 
+                  "@babel/plugin-proposal-async-generator-functions", 
+                  "@babel/plugin-proposal-private-methods"
+                ]
               }
             }
           }
